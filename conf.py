7dc14a4bd1a4e266b3cc98c6c36f5557e9275f7f
asaroyan <alexsaroyan@gmail.com>
2021-01-21 17:39:14 -0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -179,6 +179,23 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'ReadtheDocsTemplatedoc'
 
+html_theme_options = {
+    'analytics_id': 'UA-152905529-2',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
 
 # -- Options for LaTeX output ---------------------------------------------
 
