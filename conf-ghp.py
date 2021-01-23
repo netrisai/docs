f527f2171ec2758f33c0d2d3d716f4e3d9c306cd
Gor Poghosyan <gor@netris.ai>
2021-01-23 04:09:59 +0400

    fixs docs (pdf/epub) publish path

--- conf-ghp.py
+++ conf-ghp.py
@@ -395,6 +395,6 @@ today_fmt = "%B %d, %Y"
 epub_basename = 'Netrisdocs'
 
 html_context['downloads'] = list()
-html_context['downloads'].append( ('pdf', '/docs/' +language+ '/' +current_version+ '/netris-docs_' +language+ '_' +current_version+ '.pdf') )
+html_context['downloads'].append( ('pdf', '/docs/docs/' +language+ '/' +current_version+ '/netris-docs_' +language+ '_' +current_version+ '.pdf') )
 
-html_context['downloads'].append( ('epub', '/docs/' +language+ '/' +current_version+ '/netris-docs_' +language+ '_' +current_version+ '.epub') )
+html_context['downloads'].append( ('epub', '/docs/docs/' +language+ '/' +current_version+ '/netris-docs_' +language+ '_' +current_version+ '.epub') )
