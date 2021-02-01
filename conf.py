8f135f8fe54ef40023e4526a0e71658aebf7d357
Gor Poghosyan <gor@netris.ai>
2021-02-01 16:10:30 +0400

    try-logo url

--- conf.py
+++ conf.py
@@ -116,6 +116,7 @@ html_theme_options = {
     'analytics_id': 'UA-152905529-2',  #  Provided by Google in your dashboard
     'analytics_anonymize_ip': False,
     'logo_only': True,
+    "logo_url": "https://www.netris.ai/",
     'display_version': True,
 #    'prev_next_buttons_location': 'bottom',
     'prev_next_buttons_location': 'both',
