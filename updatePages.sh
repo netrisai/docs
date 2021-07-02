ac26041d336e9b71b327a5f76afd1716527d0b25
Gor Poghosyan <gor@netris.ai>
2021-07-02 23:27:37 +0400

    remove custome cname

--- updatePages.sh
+++ updatePages.sh
@@ -93,8 +93,6 @@ git init
 git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git checkout -b gh-pages
 
-# Add CNAME - this is required for GitHub to know what our custom domain is
-echo "docs.netris.ai" > CNAME
 
 # add .nojekyll to the root so that github won't 404 on content added to dirs
 # that start with an underscore (_), such as our "_content" dir..
