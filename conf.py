087eb8a06aed4444d226ae3d89198cc5c947dc39
Gor Poghosyan <gor@netris.ai>
2021-10-05 12:19:09 -0700

    rm sphinx_copybutton from extensions

--- conf.py
+++ conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx_rtd_theme',
-    'sphinx_copybutton'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
