e294a09b0e94dae6111c259407b291c0ad1126cf
Gor Poghosyan <gor@netris.ai>
2023-01-04 10:43:19 +0400

    CI: Silence all safe.directory warnings

--- updatePages.sh
+++ updatePages.sh
@@ -10,6 +10,12 @@ apt-get -y install git rsync python3-pip python3-virtualenv python3-setuptools
 
 python3 -m pip install --upgrade sphinx-rtd-theme==0.5.1 importlib-metadata==4.0.1 gitpython docutils==0.16 rinohtype pygments sphinx-copybutton
 
+#######################################
+# SILENCE ALL SAFE.DIRECTORY WARNINGS #
+#######################################
+
+git config --global --add safe.directory '*'
+
 #####################
 # DECLARE VARIABLES #
 #####################
